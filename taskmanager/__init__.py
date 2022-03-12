fc316f555b75c1d251b636e7525d0d772eace50d
Grant <GrantWils.23@googlemail.com>
2022-03-12 19:23:21 +0000

    putting the basics into place tutorial

---
+++ taskmanager/__init__.py
@@ -0,0 +1,16 @@
+''' initialize our taskmanager application as a package '''
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env # noqa
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes # noqa
