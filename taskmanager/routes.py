483b9938ba3b889fbcd367ad9d7fa601af2c6e63
Grant <GrantWils.23@googlemail.com>
2022-03-13 22:11:30 +0000

    add Categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,6 @@
 ''' route files '''
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -9,3 +9,20 @@ from taskmanager.models import Category, Task
 def home():
     ''' home returns the tasks.html template as homepage '''
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    ''' returns the categories.html when categories is called '''
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    ''' returns the add_category.html when it is called '''
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
