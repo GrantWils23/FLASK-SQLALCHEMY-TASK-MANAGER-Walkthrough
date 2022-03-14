95a6bea73f2f05b01f7e24a42250f077c9d3ae43
Grant <GrantWils.23@googlemail.com>
2022-03-14 18:26:33 +0000

    add update Categories content

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -27,3 +27,15 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    ''' returns the edit_category when a category is selected '''
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
