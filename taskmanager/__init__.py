17d458d5240885398df56a42145e434e106baf1d
Grant <GrantWils.23@googlemail.com>
2022-03-23 18:24:19 +0000

    update the init.py file path like 13

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,7 +10,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-if os.environ("DEVELOPMENT") == True:
+if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL") # local
 else:
     uri = os.environ.get("DATEBASE_URL")
